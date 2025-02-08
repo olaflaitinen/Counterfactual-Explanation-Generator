494771f67700c020a94457e029d2a2073f9336cc
Olaf Laitinen <olaf.laitinen@gmail.com>
2025-02-08 17:52:42 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Counterfactual Explanation Generator package.
+
+This package contains modules for loading data, training a logistic regression model,
+and generating counterfactual explanations using the Alibi library.
+"""
